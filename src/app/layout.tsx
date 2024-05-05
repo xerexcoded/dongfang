fbe45f773ba6d3cc7da2c9e3202135d4d8858905
xerexoded <arnavbanerjee61@gmail.com>
2024-05-06 01:44:25 +0530

    footer and main page basic scaffold

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import Header from "@/components/component/header";
+import Footer from "@/components/component/footer";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
       )}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
+import Homepage from "@/components/component/home";
 export default function Home() {
   return (
     <main className="m-4">
-      <h1 className=" flex text-3xl font-bold items-center">Dong Fang</h1>
+      <Homepage />
     </main>
   );
 }
