461f37f223f06b307f3d6811f97ce31c2112953c
xerexoded <arnavbanerjee61@gmail.com>
2024-05-06 02:12:10 +0530

    fixed linting

--- src/components/component/header.tsx
+++ src/components/component/header.tsx
@@ -45,7 +45,7 @@ export default function header() {
                       className="group grid h-auto w-full items-center justify-start gap-1 rounded-md bg-white p-4 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
                       href="#"
                     >
-                      <div className="text-sm font-medium leading-none group-hover:underline">Chef's Special</div>
+                      <div className="text-sm font-medium leading-none group-hover:underline">Chef&apos;s Special</div>
                       <div className="line-clamp-2 text-sm leading-snug text-gray-500 dark:text-gray-400">
                         A unique dish created by our talented chefs.
                       </div>
@@ -114,7 +114,7 @@ export default function header() {
                       </div>
                     </Link>
                     <Link className="group grid h-auto w-full justify-start gap-1" href="#">
-                      <div className="text-sm font-medium leading-none group-hover:underline">Chef's Special</div>
+                      <div className="text-sm font-medium leading-none group-hover:underline">Chef&apos;s Special</div>
                       <div className="line-clamp-2 text-sm leading-snug text-gray-500 dark:text-gray-400">
                         A unique dish created by our talented chefs.
                       </div>
