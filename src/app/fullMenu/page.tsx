8c3156664903d94a84b313380bdafcb780adf643
xerexoded <arnavbanerjee61@gmail.com>
2024-05-23 02:17:21 +0530

    fixed linting error in full menu page

--- src/app/fullMenu/page.tsx
+++ src/app/fullMenu/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState } from "react";
 
-const fullMenu = () => {
-  const [pdfSrc, setPdfSrc] = useState("/fullmenu/fullmenu.pdf");
+const FullMenu = () => {
+  const [pdfSrc, setPdfSrc] = useState("/FullMenu/FullMenu.pdf");
 
   const showPDF = (pdf: React.SetStateAction<string>) => {
     setPdfSrc(pdf);
@@ -12,19 +12,19 @@ const fullMenu = () => {
       <div className="mt-10 px-2 flex space-x-4 mx-auto">
         <button
           className="bg-red-700 text-white px-4 py-2 rounded hover:bg-red-700"
-          onClick={() => showPDF("/fullmenu/fullmenu.pdf")}
+          onClick={() => showPDF("/FullMenu/FullMenu.pdf")}
         >
           Chinese Menu
         </button>
         <button
           className="bg-red-700 text-white px-4 py-2 rounded hover:bg-red-700"
-          onClick={() => showPDF("/fullmenu/fullmenu.pdf")}
+          onClick={() => showPDF("/FullMenu/FullMenu.pdf")}
         >
           French Menu
         </button>
         <button
           className="bg-red-700 text-white px-4 py-2 rounded hover:bg-red-700"
-          onClick={() => showPDF("/fullmenu/fullmenu.pdf")}
+          onClick={() => showPDF("/FullMenu/FullMenu.pdf")}
         >
           English Menu
         </button>
@@ -43,4 +43,4 @@ const fullMenu = () => {
   );
 };
 
-export default fullMenu;
+export default FullMenu;
